3b52514d9204ae746ba9ce20d84c5f7ae6f2b888
CodeGunner1 <codegunnerab@gmail.com>
2025-12-08 20:11:56 -0400

    Add files via upload

---
+++ FINAL_sg MATRICES/repechaje_si/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace repechaje_si
+{
+    public partial class Form1 : Form
+    {
+        Matriz x1, x2, x3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x1 = new Matriz();
+            x2 = new Matriz();
+            x3 = new Matriz();
+
+        }
+
+
+        //cargar x1
+        private void cargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            x1.CargarRandom(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text), int.Parse(textBox4.Text));
+            textBox5.Text = x1.Descargar();
+        }
+
+        private void descargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = x1.Descargar();
+        }
+        //cargar x2
+        private void cargarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void descargarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+
+        //descargar x3
+        private void descargarToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void ejercicio1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void ejercicio2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+
+        private void mayorFrecuanciaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //aqui pone la llamada
+
+            //LLAMADA
+            int ele = 0, frec = 0;
+
+            x1.MayorFrecuenciaTriangularInferiorIzq(ref ele, ref frec);
+
+            textBox7.Text = "ele = " + ele + "   frec = " + frec;
+
+        }
+    }
+}
---
+++ FINAL_sg MATRICES/repechaje_si/Matriz.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repechaje_si
+{
+    class Matriz
+    {
+
+        //atributos
+        const int MAXF = 50;
+        const int MAXC = 50;
+
+        private int[,] x;
+
+        private int f, c;
+
+        //contructor
+
+        public Matriz()
+        {
+
+            x = new int[MAXF, MAXC];
+            f = 0;
+            c = 0;
+
+        }
+
+        //cargar y descargar
+
+        public void CargarRandom(int nf, int nc, int a, int b)
+
+        {
+            int f1, c1;
+            Random r = new Random();
+            f = nf;
+            c = nc;
+
+
+            for (f1 = 1; f1 <= f; f1++)
+
+            {
+                for (c1 = 1; c1 <= c; c1++)
+
+                {
+                    x[f1, c1] = r.Next(a, b + 1);
+
+                }
+
+            }
+
+        }
+
+
+        public string Descargar()
+
+        {
+            string s = "";
+            int f1, c1;
+            for (f1 = 1; f1 <= f; f1++)
+
+            {
+                for (c1 = 1; c1 <= c; c1++)
+
+                {
+                    s = s + x[f1, c1] + "\x9";
+
+
+                }
+                s = s + "\xd" + "\xa";
+
+            }
+
+            return s;
+
+        }
+
+
+        //examen
+
+        //pregunta 1 - Matrices
+        public void MayorFrecuenciaTriangularInferiorIzq(ref int ele, ref int frec)
+        {
+            ele = 0;
+            frec = 0;
+
+       
+            for (int f1 = 1; f1 <= f; f1++)
+            {
+                for (int c1 = 1; c1 <= f1; c1++)
+                {
+                    int actual = x[f1, c1];
+                    int cont = 0;
+
+                    
+                    for (int f2 = 1; f2 <= f; f2++)
+                    {
+                        for (int c2 = 1; c2 <= f2; c2++)
+                        {
+                            if (x[f2, c2] == actual)
+                                cont++;
+                        }
+                    }
+
+                    if (cont > frec)
+                    {
+                        frec = cont;
+                        ele = actual;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+    }
+}
